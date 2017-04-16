bd0a95ac875cd8a9e8964ff928faaf76c00751f4
Tsung_Yi_Kao <solomonjoeykao@gmail.com>
2017-04-16 23:29:45 +0800

    first homework

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php if($_POST["name"]!=""){echo $_POST["name"]; }?><br>
+Your email address is: <?php  echo $_POST["email"]; ?>
+
+
+</body>
+</html>
---
+++ welcome_get.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+
+Welcome <?php if($_GET["name"]!=""){echo $_GET["name"]; }?><br>
+Your email address is: <?php if($_GET["name"]!=""){echo $_GET["email"];} ?>
+
+
+
+</body>
+</html>
